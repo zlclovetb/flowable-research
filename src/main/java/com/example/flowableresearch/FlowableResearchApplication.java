92b5d2ed24615be5e0555c9526610234dba772db
Spring.Zhang <job_paradm@outlook.com>
2021-11-16 10:34:44 +0800

    Add files via upload
    first upload

---
+++ src/main/java/com/example/flowableresearch/FlowableResearchApplication.java
@@ -0,0 +1,15 @@
+package com.example.flowableresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class FlowableResearchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlowableResearchApplication.class, args);
+  }
+
+}
